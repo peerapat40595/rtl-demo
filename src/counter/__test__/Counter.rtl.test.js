24857351a83c8e61598607909920c19191cc2048
Peerapat Junkhaow <peerapat40595@gmail.com>
2019-10-08 13:13:57 +0700

    Add toBeInDocument

--- src/counter/__test__/Counter.rtl.test.js
+++ src/counter/__test__/Counter.rtl.test.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { render, fireEvent, cleanup, getByText } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import Counter from '../Counter'
-import ReactDOM from 'react-dom'
 
 describe('Counter', () => {
   afterEach(cleanup);
   test('show "0" as default', () => {
-    const { queryByText } = render(<Counter />)
-    expect(queryByText('0')).not.toBeNull()
+    const { getByText } = render(<Counter />)
+    expect(getByText('0')).toBeInTheDocument()
   })
   test('count up when user clicks "+"', () => {
-    const { queryByText, getByText } = render(<Counter />)
+    const { getByText } = render(<Counter />)
     fireEvent.click(getByText('+'))
-    expect(queryByText('1')).not.toBeNull()
+    expect(getByText('1')).toBeInTheDocument()
   })
   test('count down when user clicks "-"', () => {
-    const { queryByText, getByText } = render(<Counter />)
+    const { getByText } = render(<Counter />)
     fireEvent.click(getByText('-'))
-    expect(queryByText('-1')).not.toBeNull()
+    expect(getByText('-1')).toBeInTheDocument()
   })
 });
